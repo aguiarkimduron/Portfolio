4c170efdbf81b62fc6659bddbe055e57e2d6da75
kimaguilar <aguilarkimduron@gmail.com>
2020-11-08 19:33:20 +0800

    fixed sizing of image in section-works

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-const worksImg = document.querySelectorAll('.works-img-hover')
-worksImg.forEach(img => {
-    img.addEventListener('mouseover', () =>  {
-        img.style.opacity = 1;
-    })
-    img.addEventListener('mouseout', () =>  {
-        img.style.opacity = 0;
-    })
-})
+// const worksImgHover = document.querySelectorAll('.works-img-hover')
+// const worksImg = document.querySelectorAll('.works-img > img')
+// worksImgHover.forEach(img => {
+//     img.addEventListener('mouseover', () =>  {
+//         img.parentElement.children[0].classList.add('darken')
+//         img.style.opacity = 1;
+//     })
+//     img.addEventListener('mouseout', () =>  {
+//         img.parentElement.children[0].classList.remove('darken')
+//         img.style.opacity = 0;
+//     })
+// })
+// worksImg.forEach(img => {
+//     img.addEventListener('mouseover', () => {
+//         img.parentElement.children[1].style.opacity = 1
+//     })
+//     img.addEventListener('mouseout', () => {
+//         img.parentElement.children[1].style.opacity = 0
+//     })
+// })
